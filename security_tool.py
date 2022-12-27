e73a1bd959b513fe82df401fe6d799b855a7474a
Muhammed Mustafa Tetikoğlu <97004775+F4KOR4LL@users.noreply.github.com>
2022-12-27 20:42:15 +0300

    Büyük Güncelleme Geldi

--- security_tool.py
+++ security_tool.py
@@ -30,14 +30,75 @@ def main():
 if __name__ == "__main__":
   main()
 
+def scan_network(network, subnet):
+  # Ağınızdaki cihazları taramak için bir aralık belirleyin
+  start = 1
+  end = 254
+  devices = []
+  
+  # Ağınızdaki cihazları taramaya başlayın
+  for host in range(start, end+1):
+    # Her cihaza bir ping gönderin
+    address = network + str(host)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+
+    # Eğer cihaz yanıt verirse, cihazın IP adresini ve cihaz türünü ekrana yazdırın
+    if s.connect_ex((address, 135)):
+      print(address + " çalışıyor")
+      # Cihaz türünü tespit etmek için nmap kullanın
+      cmd = "nmap -O " + address
+      proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+      result = proc.stdout.read()
+      device_type = result.decode("utf-8").split("\n")[-2]
+      
+      # Cihaz üzerinde açık portları tespit etmek için nmap kullanın
+      cmd = "nmap -p- " + address
+      proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+      result = proc.stdout.read()
+      open_ports = result.decode("utf-8").split("\n")[-2]
+      
+      # Cihaz üzerinde çalışan servisleri tespit etmek için nmap kullanın
+      cmd = "nmap -sV " + address
+      proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+      result = proc.stdout.read()
+      services = result.decode("utf-8").split("\n")[-2]
+      
+      # Cihaz üzerinde güncel olmayan yazılım ve servisler tespit etmek için nessus kullanın
+      cmd = "nessus " + address
+      proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+      result = proc.stdout.read()
+      print(result)
+    else:
+      print(address + " çalışmıyor")
+#Tarama işlemini bitirin
+print("Tarama tamamlandı.")
+sys.exit()
+
+
+
 
 # Ağınızın IP adresini ve alt ağ maskesini girin
 network = "192.168.1.0"
 subnet = "255.255.255.0"
 
-# Ağınızdaki cihazları taramak için bir aralık belirleyin
-start = 1
-end = 254
+def display_device_info(devices):
+  for device in devices:
+    print("IP adresi: " + device["ip_address"])
+    print("Cihaz türü: " + device["device_type"])
+    print("Açık portlar: " + device["open_ports"])
+    print("Servisler: " + device["services"])
+    print("Güncel olmayan yazılım ve servisler: " + device["outdated_software"])
+    print("-------------------")
+
+def generate_report(devices):
+  report_file = open("report.txt", "w")
+  for device in devices:
+    report_file.write("IP adresi: " + device["ip_address"] + "\n")
+    report_file.write("Cihaz türü: " + device["device_type"] + "\n")
+    report_file.write("Açık portlar: " + device["open_ports"] + "\n")
+    report_file.write("Servisler: " + device["services"] + "\n")
+
 
 # Ağınızdaki cihazları taramaya başlayın
 for host in range(start, end+1):
